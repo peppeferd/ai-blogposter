8276ab0161452150f484b85b3ef1637d425224e6
peppeferd <giuseppeferlazzo@outlook.it>
2023-11-29 07:47:45 +0100

    A New Commit

---
+++ src/app/api/test/route.ts
@@ -0,0 +1,16 @@
+import { NextRequest, NextResponse } from "next/server";
+import { connectToDatabase } from "@/lib/mongo";
+import { withApiAuthRequired } from "@auth0/nextjs-auth0";
+
+const withApiAuthRequiredExtended = withApiAuthRequired as any;
+export const GET = withApiAuthRequiredExtended(
+  async (request: NextRequest, response: NextResponse) => {
+    /*   const { db } = await connectToDatabase();
+     */ try {
+      /*     const test = await db.collection("test").find({}).toArray();
+       */ return NextResponse.json({ message: "Hello World" }, { status: 200 });
+    } catch (error) {
+      return NextResponse.error();
+    }
+  }
+);
