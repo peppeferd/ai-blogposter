a7ee75b4e7542e157334d6bfdf3ebba90fd3e0b0
peppeferd <giuseppeferlazzo@outlook.it>
2024-04-05 05:56:19 +0200

    Last one

---
+++ src/app/new/layout.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+export const metadata = {
+  title: "New post | Bloggify",
+  description: "AI Next js - Create post",
+};
+
+const layout = ({ children }: { children: React.ReactNode }) => {
+  return <>{children}</>;
+};
+export default layout;
---
+++ src/data/tones.ts
@@ -0,0 +1,24 @@
+export const tones: Tone[] = [
+  {
+    label: "Neutral",
+    value: "neutral",
+  },
+  {
+    label: "Happy",
+    value: "happy, optimistic, positive,and cheerful",
+  },
+  {
+    label: "Sad",
+    value: "sad, pessimistic, negative, and gloomy",
+  },
+  {
+    label: "Angry",
+    value:
+      "angry and annoyed, frustrated, or irritated, using strong language and exclamation marks",
+  },
+  {
+    label: "Formal",
+    value:
+      "formal, professional, and serious, using posh and formal language and no contractions",
+  },
+];
